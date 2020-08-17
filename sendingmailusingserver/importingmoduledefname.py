0b30e3b69853ec074d9eb84c26038a711ae38c55
Ritik <54552284+ritiksinghsahib@users.noreply.github.com>
2020-08-18 00:18:39 +0530

    Add files via upload

---
+++ sendingmailusingserver/importingmoduledefname.py
@@ -0,0 +1,21 @@
+#Importing the necessary modules
+import difflib
+import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from netmiko import ConnectHandler
+ 
+ 
+#Defining the device to monitor
+ip = '10.10.10.2'
+ 
+#Defining the device type for running netmiko
+device_type = 'arista_eos'
+ 
+#Defining the username and password for running netmiko
+username = 'admin'
+password = 'python'
+ 
+#Defining the command to send to each device
+command = 'show running'
